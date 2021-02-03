fdeba15d65a5938479562e1afe213848a335738a
iluvata <1124026683@qq.com>
2021-02-03 14:24:54 +0800

    new post automate deploy on Wed 03 Feb 2021 02:24:54 PM HKT

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@
 hugo
 cd public
 git add .
-git commit -m "new post"
+git commit -m "new post automate deploy on $(date -d 'now')"
 git push
 cd ..
 git add .
-git commit -m "new post"
+git commit -m "new post automate deploy on $(date -d 'now')"
 git push
