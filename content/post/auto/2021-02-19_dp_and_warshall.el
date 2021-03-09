a6b624590e83af9a98695535fca7b73e1c894233
Iluvata <1124026683@qq.com>
2021-03-09 11:31:38 +0800

    new euler curcuit post

---
+++ content/post/auto/2021-02-19_dp_and_warshall.el
@@ -0,0 +1,44 @@
+(TeX-add-style-hook
+ "2021-02-19_dp_and_warshall"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref")
+   (LaTeX-add-labels
+    "sec:org7981eda"
+    "sec:org111e7d9"
+    "sec:org9db1458"
+    "sec:org07d6980"
+    "sec:org381c759"
+    "sec:org0d5d661"
+    "sec:orgbe19002"
+    "sec:orgd0f2a4c"
+    "sec:org93052a5"))
+ :latex)
+
